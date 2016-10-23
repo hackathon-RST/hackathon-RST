7eba541bd9fa237bb47739a73f94a9afc5defa7b
Stefanie Moy <ssmores@hotmail.com>
2016-10-23 16:51:13 +0000

    Model updated so it would create upon running file.  Seed file updated so it would populate with sample data.  Server has placeholder routes.

--- seed.py
+++ seed.py
@@ -11,7 +11,7 @@ db.create_all()
 def load_users():
 	"""load all users from seed file"""
 	with open('./seed_data/users.tsv','r+') as data:
-		for i, in row in enumerate(data):
+		for row in data:
 			row = row.rstrip()
 			user_name, password, user_type_id = row.split("\t")
 
@@ -24,7 +24,7 @@ def load_users():
 def load_user_type():
 	"""load two types of users"""
 	with open('./seed_data/user_type.tsv', 'r+') as data:
-		for i, in row in enumerate(data):
+		for row in data:
 			row = row.rstrip()
 			user_type = row.split("\t")
 
@@ -37,29 +37,30 @@ def load_companies():
 	"""load companies into database"""
 
 	with open('./seed_data/companies.tsv', 'r+') as data:
-		for i, in row in enumerate(data):
+		for row in data:
 			row = row.rstrip()
-			companies = Companies(name= name,
-								address= address,
-								hr_contact_info= hr_contact_info)
+			name, address, hr_contact_info = row.split('\t')
+			companies = Companies(name=name,
+								address=address,
+								hr_contact_info=hr_contact_info)
 			db.session.add(companies)
 			db.session.commit()
 
 def load_incidents():
 	"""load all Incidents"""
 	with open('./seed_data/incidents.tsv', 'r+') as data:
-		for i, in row in enumerate(data):
+		for row in data:
 			row = row.rstrip()
 			incidents = Incidents(user_id=user_id,
-								company_id= company_id,
-								reported_at= reported_at,
+								company_id=company_id,
+								reported_at=reported_at,
 								transaction_id=transaction_id)
 			db.session.add(incidents)
 			db.session.commit()
 
 if __name__ == '__main__':
-	load_users()
 	load_user_type()
+	load_users()
 	load_companies()
-	load_incidents()
+	# load_incidents()
 
